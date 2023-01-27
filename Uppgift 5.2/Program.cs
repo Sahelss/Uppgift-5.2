f5c5f7b439b3afa701ced6b861911960f7f9d128
EEDAT <EEDAT@TEB114101>
2023-01-27 14:21:20 +0100

    Add project files.

---
+++ Uppgift 5.2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Uppgift_5._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in 3 heltal");
+            int[] nmr = new int[3];
+
+            nmr[0] = int.Parse(Console.ReadLine());
+            nmr[1] = int.Parse(Console.ReadLine());
+            nmr[2] = int.Parse(Console.ReadLine());
+            {
+                Console.WriteLine("Här är dina 3 heltal plusade med varandra: " + (nmr[0] + nmr[1] + nmr[2]));
+            }
+        }
+    }
+}
+
